04ad7b788478db33db566612c9c5fb26f6bf7f45
CLAUDIO BAHAMONDE <cpbahamonde@gmail.com>
2022-04-25 03:15:03 -0400

    20220425-03.15 Actualiza Carpetas full

--- first_app/.history/first_app/app/controllers/pages_controller_20220419001516.rb
+++
@@ -1,9 +0,0 @@
-class PagesController < ApplicationController
-  def one
-  end
-
-  def two
-  end
-
-  
-end
--- first_app/.history/first_app/config/routes_20220419000512.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get 'pages/one'
-  get 'pages/two'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
-  
-end
