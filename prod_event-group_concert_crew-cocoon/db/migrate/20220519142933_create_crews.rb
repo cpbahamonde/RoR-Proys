26b685b0f3a2bf5a27f4763691323c311899dcd7
CLAUDIO BAHAMONDE <cpbahamonde@gmail.com>
2022-05-25 11:43:34 -0400

    intentando arreglar cocoon

---
+++ prod_event-group_concert_crew-cocoon/db/migrate/20220519142933_create_crews.rb
@@ -0,0 +1,10 @@
+class CreateCrews < ActiveRecord::Migration[7.0]
+  def change
+    create_table :crews do |t|
+      t.string :name
+      t.references :group, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
