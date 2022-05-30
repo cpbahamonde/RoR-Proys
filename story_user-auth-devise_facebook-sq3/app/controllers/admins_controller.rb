40e6c89cb1c42c1ce939074f4177b5089eea797c
CLAUDIO BAHAMONDE <99505746+cpbahamonde@users.noreply.github.com>
2022-05-30 14:37:26 -0400

    Delete story_user-auth-devise_facebook-sq3 directory

--- story_user-auth-devise_facebook-sq3/app/controllers/admins_controller.rb
+++
@@ -1,9 +0,0 @@
-class AdminsController < ApplicationController
-  def index
-    if current_user.admin?
-      @users = User.all
-    else
-      redirect_to :root, alert: "Only admins are allowed in this section"
-    end
-  end
-end
